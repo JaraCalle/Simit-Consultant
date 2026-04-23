22166cbae6f31365b46117a142baf73b0d7c432f
Juan Jara <jaracalle2041@gmail.com>
2026-04-23 00:25:38 -0500

    Refactor (Backend-main): Sort the imports order

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,8 +1,8 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
-from app.core.config import settings
 from app.api import simit
+from app.core.config import settings
 
 app = FastAPI(
     title=settings.PROJECT_NAME,
